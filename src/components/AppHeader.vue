c66c125ed453741f639457c8dd95f8bec2668bc0
chinel <nelokwaz@gmail.com>
2024-02-20 06:47:52 +0100

    feat: add the logic to toggle auth modal using the store

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,9 @@
         <ul class="flex flex-row mt-1">
           <!-- Navigation Links -->
           <li>
-            <a class="px-2 text-white" href="#">Login / Register</a>
+            <a class="px-2 text-white" href="#" @click.prevent="toggleAuthModal"
+              >Login / Register</a
+            >
           </li>
           <li>
             <a class="px-2 text-white" href="#">Manage</a>
@@ -22,7 +24,19 @@
 </template>
 
 <script>
+import { mapStores, mapState, mapWritableState } from 'pinia'
+import useModalStore from '@/stores/modal'
 export default {
-  name: 'AppHeader'
+  name: 'AppHeader',
+  computed: {
+    ...mapStores(useModalStore),
+    ...mapWritableState(useModalStore, ['isOpen'])
+  },
+  methods: {
+    toggleAuthModal() {
+      this.isOpen = !this.isOpen
+      console.log(this.isOpen)
+    }
+  }
 }
 </script>
