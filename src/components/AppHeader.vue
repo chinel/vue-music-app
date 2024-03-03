9851b400c71d7634946c5b10ab990bbadb3e7a2e
chinel <nelokwaz@gmail.com>
2024-03-03 13:33:17 +0100

    feat: add names to routes and add catch all routes

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,13 +4,18 @@
     <nav class="container mx-auto flex justify-between items-center py-5 px-4">
       <!-- App Name -->
       <div>
-        <router-link class="text-white font-bold uppercase text-2xl mr-4" to="/">Music</router-link>
+        <router-link
+          class="text-white font-bold uppercase text-2xl mr-4"
+          :to="{ name: 'home' }"
+          exact-active-class="no-active"
+          >Music</router-link
+        >
       </div>
       <div class="flex items-center">
         <!-- Primary Navigation -->
         <ul class="flex flex-row mt-1">
           <li>
-            <router-link class="px-2 text-white" to="/about">About</router-link>
+            <router-link class="px-2 text-white" :to="{ name: 'about' }">About</router-link>
           </li>
           <!-- Navigation Links -->
           <li v-if="!userStore.userLoggedIn">
@@ -20,7 +25,7 @@
           </li>
           <template v-else>
             <li>
-              <router-link class="px-2 text-white" to="/manage">Manage</router-link>
+              <router-link class="px-2 text-white" :to="{ name: 'manage' }">Manage</router-link>
             </li>
             <li>
               <a class="px-2 text-white" href="#" @click.prevent="userStore.signout">Logout</a>
--- src/router/index.js
+++ src/router/index.js
@@ -6,22 +6,39 @@ import Manage from '@/views/Manage.vue'
 const routes = [
   {
     path: '/',
-    component: Home
+    component: Home,
+    name: 'home'
   },
   {
     path: '/about',
-    component: About
+    component: About,
+    name: 'about'
+  },
+  {
+    path: '/manage-music',
+    // alias: '/manage',
+    component: Manage,
+    name: 'manage'
   },
   {
     path: '/manage',
-    component: Manage
+    redirect: {
+      name: 'manage'
+    }
+  },
+  {
+    path: '/:catchAll(.*)*',
+    redirect: {
+      name: 'home'
+    }
   }
 ]
 
 // the createWebHistory function uses the History Api of the browser under the hood
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL), //this enables history
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
