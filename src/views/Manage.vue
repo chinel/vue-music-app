88578000b0e6562d07f5b934dc2513a5e73e6993
chinel <nelokwaz@gmail.com>
2024-03-05 07:55:56 +0100

    fix: add navigation guard to the manage music route

--- src/views/Manage.vue
+++ src/views/Manage.vue
@@ -135,3 +135,21 @@
     </div>
   </section>
 </template>
+
+<script>
+import useUserStore from '@/stores/user'
+export default {
+  name: 'Manage',
+  beforeRouteEnter(to, from, next) {
+    const store = useUserStore()
+
+    if (store.userLoggedIn) {
+      next()
+    } else {
+      next({
+        name: 'home'
+      })
+    }
+  }
+}
+</script>
