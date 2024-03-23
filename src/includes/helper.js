4d43ebeb90037200522dc945060a3fa75473a0ef
chinel <nelokwaz@gmail.com>
2024-03-23 19:56:58 +0100

    feat: add logic to play and pause music, delete dummy template

---
+++ src/includes/helper.js
@@ -0,0 +1,9 @@
+export default {
+  formatTime(time) {
+    console.log(time)
+    // time passed is in seconds
+    const minutes = Math.floor(time / 60) || 0 // We used Math.floor because we do not want decimal values
+    const seconds = Math.round(time - minutes * 60 || 0)
+    return `${minutes}:${seconds < 10 ? 0 : ''}${seconds}`
+  }
+}
