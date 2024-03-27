98b701440a812415af7d6ea425820ca4cd0c8747
chinel <nelokwaz@gmail.com>
2024-03-27 08:43:25 +0100

    feat: add transitioning to router view components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <app-header />
-  <router-view></router-view>
+  <router-view v-slot="{ Component }">
+    <transition name="fade">
+      <component :is="Component"></component>
+    </transition>
+  </router-view>
   <app-player />
   <app-auth />
 </template>
@@ -28,3 +32,18 @@ export default {
   }
 }
 </script>
+
+<style>
+.fade-enter-from {
+  opacity: 0;
+}
+
+.fade-enter-active {
+  transition: all 0.5s linear;
+}
+
+.fade-leave-to {
+  transition: all 0.5s linear;
+  opacity: 0;
+}
+</style>
