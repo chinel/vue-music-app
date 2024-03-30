a8d200ae34af0a7e32f977c161a0549e4d5dfb30
chinel <nelokwaz@gmail.com>
2024-03-30 14:14:12 +0100

    feat: add english and french translations

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,7 +8,7 @@
       ></div>
       <div class="container mx-auto">
         <div class="text-white main-header-content">
-          <h1 class="font-bold text-5xl mb-5">Listen to Great Music!</h1>
+          <h1 class="font-bold text-5xl mb-5">{{ $t('home.listen') }}</h1>
           <p class="w-full md:w-8/12 mx-auto">
             Feeling stressed? Let the power of melody wash over you. It's time to Listen to Great
             Music!
