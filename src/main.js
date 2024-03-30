e212a8a095a7a3e107e6a60ca0824ac6ebff854c
chinel <nelokwaz@gmail.com>
2024-03-30 09:23:41 +0100

    feat: create custom icon directives

--- src/main.js
+++ src/main.js
@@ -9,13 +9,15 @@ import router from './router'
 import VeeValidatePlugin from './includes/validation'
 import { auth } from './includes/firebase'
 
+import Icon from './directives/icons'
+
 let app
 
 auth.onAuthStateChanged(() => {
   //check if app is undefined and only initialize the app if undefined
   if (!app) {
     app = createApp(App)
-
+    app.directive('icon', Icon)
     app.use(createPinia())
     app.use(router)
     app.use(VeeValidatePlugin, { foo: 'hello' })
