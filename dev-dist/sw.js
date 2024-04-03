17522afe182a8902ca68ac1c1c25d8f27c3a64bf
chinel <nelokwaz@gmail.com>
2024-04-03 07:34:34 +0100

    feat: update/change fields in manifest file

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.da9jclfm4fg"
+    "revision": "0.pba2mlrpckg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
