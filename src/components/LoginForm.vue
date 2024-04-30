1322dad5b68da90c03cc18ffcfb0448bff62802a
chinel <nelokwaz@gmail.com>
2024-04-30 05:20:18 +0100

    fix: add missing translation

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -20,7 +20,7 @@
     </div>
     <!-- Password -->
     <div class="mb-3">
-      <label class="inline-block mb-2">Password</label>
+      <label class="inline-block mb-2">{{ $t('loginForm.passwordLabel') }}</label>
       <vee-field
         type="password"
         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
