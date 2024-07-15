2a0a837550e6f9cc9bc1198c32dc5a2b391fcf7e
chinel <nelokwaz@gmail.com>
2024-07-15 14:42:41 +0100

    fix: add missing translation

--- src/components/RegisterForm.vue
+++ src/components/RegisterForm.vue
@@ -58,7 +58,7 @@
     </div>
     <!-- Confirm Password -->
     <div class="mb-3">
-      <label class="inline-block mb-2">Confirm Password</label>
+      <label class="inline-block mb-2">{{ $t('register.confirmPassword') }}</label>
       <vee-field
         type="password"
         class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
